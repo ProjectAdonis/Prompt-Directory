507e158c7374e882a1273f8c309819d3c76203c8
CyberJoshh <pierrelouis.camaret@gmail.com>
2023-07-08 15:39:19 +0200

    Update js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
   var $container = $('.container');
   var $leftSection = $('.one-half.left');
   var $rightSection = $('.one-half.right');
@@ -6,95 +6,75 @@ $(document).ready(function() {
   var $h1r = $('.h1-r');
 
   $leftSection.click(function() {
-    if ($container.hasClass('left-is-hovered')) {
-      $container.removeClass('left-is-hovered');
-    } else {
-      $container.removeClass('right-is-hovered');
-      $container.addClass('left-is-hovered');
-    }
+    $container.toggleClass('left-is-hovered', !$container.hasClass('left-is-hovered'));
+    $container.removeClass('right-is-hovered');
   });
 
   $rightSection.click(function() {
-    if ($container.hasClass('right-is-hovered')) {
-      $container.removeClass('right-is-hovered');
-    } else {
-      $container.removeClass('left-is-hovered');
-      $container.addClass('right-is-hovered');
-    }
+    $container.toggleClass('right-is-hovered', !$container.hasClass('right-is-hovered'));
+    $container.removeClass('left-is-hovered');
   });
 
   function adjustSectionSize() {
     var screenWidth = $(window).width();
-    if (screenWidth > 768) {
-      $leftSection.off('mouseenter mouseleave').hover(
-        function() {
-          if (!$container.hasClass('left-is-hovered')) {
-            $leftSection.css('width', '70%');
-            $rightSection.css('width', '30%');
-            $h1l.css('font-size', '3vw');
-            $h1r.css('font-size', '2vw');
-          }
-        },
-        function() {
-          if (!$container.hasClass('left-is-hovered')) {
-            $leftSection.css('width', '50%');
-            $rightSection.css('width', '50%');
-            $h1l.css('font-size', '2vw');
-            $h1r.css('font-size', '2vw');
-          }
+    var isLargeScreen = screenWidth > 768;
+
+    if (isLargeScreen) {
+      $leftSection.off('mouseenter mouseleave').hover(function() {
+        if (!$container.hasClass('left-is-hovered')) {
+          $leftSection.css({ 'width': '70%', 'filter': 'brightness(100%)' });
+          $rightSection.css({ 'width': '30%', 'filter': 'brightness(50%)' });
+          $h1l.css('font-size', '3vw');
+          $h1r.css('font-size', '2vw');
         }
-      );
-
-      $rightSection.off('mouseenter mouseleave').hover(
-        function() {
-          if (!$container.hasClass('right-is-hovered')) {
-            $rightSection.css('width', '70%');
-            $leftSection.css('width', '30%');
-            $h1r.css('font-size', '3vw');
-            $h1l.css('font-size', '2vw');
-          }
-        },
-        function() {
-          if (!$container.hasClass('right-is-hovered')) {
-            $rightSection.css('width', '50%');
-            $leftSection.css('width', '50%');
-            $h1l.css('font-size', '2vw');
-            $h1r.css('font-size', '2vw');
-          }
+      }, function() {
+        if (!$container.hasClass('left-is-hovered')) {
+          $leftSection.css({ 'width': '50%', 'filter': 'brightness(100%)' });
+          $rightSection.css({ 'width': '50%', 'filter': 'brightness(100%)' });
+          $h1l.css('font-size', '2vw');
+          $h1r.css('font-size', '2vw');
         }
-      );
+      });
 
-
+      $rightSection.off('mouseenter mouseleave').hover(function() {
+        if (!$container.hasClass('right-is-hovered')) {
+          $rightSection.css({ 'width': '70%', 'filter': 'brightness(100%)' });
+          $leftSection.css({ 'width': '30%', 'filter': 'brightness(50%)' });
+          $h1r.css('font-size', '3vw');
+          $h1l.css('font-size', '2vw');
+        }
+      }, function() {
+        if (!$container.hasClass('right-is-hovered')) {
+          $rightSection.css({ 'width': '50%', 'filter': 'brightness(100%)' });
+          $leftSection.css({ 'width': '50%', 'filter': 'brightness(100%)' });
+          $h1l.css('font-size', '2vw');
+          $h1r.css('font-size', '2vw');
+        }
+      });
     } else {
-      $leftSection.off('mouseenter mouseleave').hover(
-        function() {
-          if (!$container.hasClass('left-is-hovered')) {
-            $leftSection.css('height', '60%');
-            $rightSection.css('height', '40%');
-          }
-        },
-        function() {
-          if (!$container.hasClass('left-is-hovered')) {
-            $leftSection.css('height', '50%');
-            $rightSection.css('height', '50%');
-          }
+      $leftSection.off('mouseenter mouseleave').hover(function() {
+        if (!$container.hasClass('left-is-hovered')) {
+          $leftSection.css({ 'height': '60%', 'filter': 'brightness(100%)' });
+          $rightSection.css({ 'height': '40%', 'filter': 'brightness(50%)' });
         }
-      );
+      }, function() {
+        if (!$container.hasClass('left-is-hovered')) {
+          $leftSection.css({ 'height': '50%' });
+          $rightSection.css({ 'height': '50%' });
+        }
+      });
 
-      $rightSection.off('mouseenter mouseleave').hover(
-        function() {
-          if (!$container.hasClass('right-is-hovered')) {
-            $rightSection.css('height', '60%');
-            $leftSection.css('height', '40%');
-          }
-        },
-        function() {
-          if (!$container.hasClass('right-is-hovered')) {
-            $rightSection.css('height', '50%');
-            $leftSection.css('height', '50%');
-          }
+      $rightSection.off('mouseenter mouseleave').hover(function() {
+        if (!$container.hasClass('right-is-hovered')) {
+          $rightSection.css({ 'height': '60%', 'filter': 'brightness(100%)' });
+          $leftSection.css({ 'height': '40%', 'filter': 'brightness(50%)' });
         }
-      );
+      }, function() {
+        if (!$container.hasClass('right-is-hovered')) {
+          $rightSection.css({ 'height': '50%' });
+          $leftSection.css({ 'height': '50%' });
+        }
+      });
     }
   }
 
