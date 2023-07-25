75fd0dd41527a1dd824dc7335abe0a6219c87c9c
CyberJoshh <pierrelouis.camaret@gmail.com>
2023-07-25 23:59:50 +0200

    bugged out everywhere

--- SD/script-sd.js
+++ SD/script-sd.js
@@ -13,19 +13,18 @@ filtersDropdown.addEventListener('change', function () {
 	  cardsDiv.style.display = 'inline-block';
 	  pageNcontentMain.style.display = 'grid';
 	  pageNcontentMain.style.transform = 'translateY(0)';
-	  header.style.transform = 'translateX(0)';
+	  header.style.transform = 'translateY(0)';
 	  break;
 	case 'positive':
 	  cardsDiv.style.display = 'inline-block';
 	  pageNcontentMain.style.display = 'none';
 	  pageNcontentMain.style.transform = 'translateY(0)';
-	  header.style.transform = 'translateX(0)';
+	  header.style.transform = 'translateY(0)';
 	  break;
 	case 'negative':
 	  cardsDiv.style.display = 'none';
 	  pageNcontentMain.style.display = 'grid';
 	  pageNcontentMain.style.transform = 'translateY(-45%)';
-	  header.style.transform = 'translateX(-1%)';
 	  break;
 	default:
 	  break;
