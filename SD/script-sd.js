dc5188c81e9729d989992a642d596002d113993c
CyberJoshh <pierrelouis.camaret@gmail.com>
2023-07-25 20:19:25 +0200

    css update + filters

--- SD/script-sd.js
+++ SD/script-sd.js
@@ -13,19 +13,19 @@ filtersDropdown.addEventListener('change', function () {
 	  cardsDiv.style.display = 'inline-block';
 	  pageNcontentMain.style.display = 'grid';
 	  pageNcontentMain.style.transform = 'translateY(0)';
-	  header.style.transform = 'translateY(0)';
+	  header.style.transform = 'translateX(0)';
 	  break;
 	case 'positive':
 	  cardsDiv.style.display = 'inline-block';
 	  pageNcontentMain.style.display = 'none';
 	  pageNcontentMain.style.transform = 'translateY(0)';
-	  header.style.transform = 'translateY(0)';
+	  header.style.transform = 'translateX(0)';
 	  break;
 	case 'negative':
 	  cardsDiv.style.display = 'none';
 	  pageNcontentMain.style.display = 'grid';
-	  pageNcontentMain.style.transform = 'translateY(-75%)';
-	  header.style.transform = 'translateY(-164%)';
+	  pageNcontentMain.style.transform = 'translateY(-45%)';
+	  header.style.transform = 'translateX(-1%)';
 	  break;
 	default:
 	  break;
